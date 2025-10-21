5ca52900afca965eb8216f7cce16f62ea51fbfaa
Fatema-Abdulla <Darksweet202@gmail.com>
2025-10-21 18:12:50 +0300

    fix the post problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css"
 function App() {
   return (
     <>
-      <main>
+      <main className="bg-dark">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/additem" element={<AddItem />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 import { GetItems } from "../services/item"
-import { Button, Card, Badge } from "react-bootstrap"
+import { Button, Card, Badge, Container, Row, Col } from "react-bootstrap"
 
 const Home = () => {
   let navigate = useNavigate()
@@ -27,25 +27,44 @@ const Home = () => {
       >
         Lost an item? Report it Missing here!
       </Button>
-      <h1>Lost Item List</h1>
-      {items.map((item) => (
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={item.img} />
-          <Card.ImgOverlay>
-            <Card.Title>{item.name}</Card.Title>
-            <Badge bg="warning" text="dark">
-              {item.status}
-            </Badge>
-          </Card.ImgOverlay>
-          <Card.Body>
-            <Card.Text text="dark">{item.description}</Card.Text>
-            <Card.Text text="info">{item.lastseen}</Card.Text>
-            <Button variant="primary" onClick={() => navigate("/")}>
-              Found
-            </Button>
-          </Card.Body>
-        </Card>
-      ))}
+      <h1 className="title-home">Lost Item List</h1>
+      <Container fluid>
+        <Row>
+          {items.map((item) => (
+            <Col key={item._id} xs={12} sm={6} md={4} lg={3}>
+              <Card style={{ width: "18rem", height: "15rem" }}>
+                <Card.Img
+                  variant="top"
+                  src={item.image}
+                  alt={`This image for item`}
+                />
+                <Card.ImgOverlay>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Badge bg="warning" text="dark">
+                    {item.status}
+                  </Badge>
+                </Card.ImgOverlay>
+                <Card.Body className="card-body-flex">
+                  <div className="card-text-grow">
+                    <Card.Text className="text-dark">
+                      {item.description || <span>&nbsp;</span>}
+                    </Card.Text>
+                    <Card.Text className="text-secondary">
+                      {item.lastSeen || <span>&nbsp;</span>}
+                    </Card.Text>
+                  </div>
+                  <Card.Text className="text-danger">
+                    {item.owner || <span>&nbsp;</span>}
+                  </Card.Text>
+                  <Button variant="primary" onClick={() => navigate("/")}>
+                    Found
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </>
   )
 }
