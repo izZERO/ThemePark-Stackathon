28cfdbd2a6d7547ddcae12f41f1f1eab6a7992cb
izZERO <crusherh7x@hotmail.com>
2025-10-22 09:47:11 +0300

    moved frontend to separate repo

--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App.jsx"
-
-createRoot(document.querySelector("#root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
